4320ea7e0b94247135d6781850e939d96d81dd52
Ann15433 <manya.lukyanchikova@yandex.com>
2026-03-31 21:33:58 +0300

    admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Phone  # замените Phone на имя вашей модели
+from .models import Phone  
 
 @admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
     exclude = ['release_date']
 
     list_display = ['name', 'price', 'image_preview']
-    search_fields = ['name']  # поиск по полю name
+    search_fields = ['name']  
 
     def image_preview(self, obj):
         if obj.image:
