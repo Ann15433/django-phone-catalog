c42ed42c5e18cd35bbd7526142827109685d8b47
Ann15433 <manya.lukyanchikova@yandex.com>
2026-03-31 21:32:46 +0300

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.org/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key-here'
+SECRET_KEY = 'my-secret-key-here'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
